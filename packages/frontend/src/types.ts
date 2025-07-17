c780120e58a8e96c541497221ec9acbb9f8de71f
Justin Gardner (Rhynorater) <justin@rhynorater.com>
2025-07-17 14:53:46 -0400

    some preliminary datatypes

--- packages/frontend/src/types.ts
+++ packages/frontend/src/types.ts
@@ -2,3 +2,33 @@ import { type Caido } from "@caido/sdk-frontend";
 import { type API } from "backend";
 
 export type FrontendSDK = Caido<API, Record<string, never>>;
+
+export type Storage = {
+  globalAgents: Agent[];
+};
+
+export type Agent = {
+  id: string;
+  name: string;
+  systemPrompt: string;
+  knowledgeBase: string[];
+  tools: Tool[];
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export type Tool = {
+  name: string;
+  parameters: Record<string, any>; // map of parameters
+  function: (parameters: Record<string, any>) => Promise<boolean>; // boolean if the tool was successful
+};
+
+export enum AgentState {
+  Paused = 'paused', // Agent has been paused. This state occurs when 
+  Restarted = 'restarted',
+  Error = 'error',
+  WaitingOnAI = 'running - waiting on ai',
+  ReadyToImplementActions = 'idle - ready to implement actions', 
+  WaitingOnReplay = 'running - waiting on replay',
+  ReadyToTellAI = 'idle - ready to tell ai'
+}
